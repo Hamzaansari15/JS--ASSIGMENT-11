9f420baf021abed53f2f3577dc4bd1faf544cc3c
Hamzaansari <hamzaansaree15@gmail.com>
2022-08-01 05:10:47 +0500

    ASSIGMENT COMPLETED

---
+++ app.js
@@ -0,0 +1,77 @@
+// function power(a,b){
+//     var a = 4;
+//     var b = 3;
+//     console.log(a*a*a);
+// }
+// power();
+// var a = prompt("Enter year");
+// function yeardetermine(a){
+//     var b = a.slice(2);
+//     if(b %4 === 0){
+//         document.write("<h1>It is a leap year</h1>");
+//     }else{
+//         document.write("<h1> It is not a leap year</h1>");
+//     }
+// }
+// yeardetermine(a);
+
+// function determineS(a,b,c){
+//     return( (a + b + c)/2 );
+// }
+// var d = determineS(6,4,8);
+// function determineAreaOfTriangle(a,b,c,d){
+//     document.write("<h1>" + (d*(d-a)*(d-b)*(d-c)) + "</h1>");
+// }
+// determineAreaOfTriangle(2,4,8,9); 
+// var firstSubject = +prompt("Enter your first subject marks");
+// var secondSubject = +prompt("Enter your second subject marks");
+// var thirdSubject = +prompt("Enter your third subject marks");
+// function average(){
+//     return((firstSubject + secondSubject + thirdSubject)/3);
+// }
+// // var marksObtained = average();
+// function percentage(){
+//     return((average()/100)*100);
+
+// }
+// function mainFunction(){
+//     document.write("<h1> The average marks is: " + average() + "</h1>");
+//     document.write("<h1> The percentage is: " + percentage() + "</h1>");
+// }
+// mainFunction();
+// var b = prompt("Find the index of words in cowebs");
+// function indexOf(){
+//     var a = "cowebs";
+//     for(i = 0; i <= a.length; i++){
+//         if(a[i] == b){
+//             return i;
+//         }else{
+//             return-1;
+//         }
+// }
+
+// }
+//  console.log(indexOf());
+// var distance = prompt("enter distance two cities");
+// function kmInMetres(){
+//     document.write("<h1> The " + distance + " KM is equal to " + distance * 1000 + " metres.</h1>");
+// }
+// kmInMetres();
+// function kmInfeet(){
+//     document.write("<h1> The " + distance + " KM is equal to " + distance * 3280 + " feets.</h1>");
+// }
+// kmInfeet();
+// function kmInInches(){
+//     document.write("<h1> The " + distance + " KM is equal to " + distance * 39370 + " Inches.</h1>");
+// }
+// kmInInches();
+// function kmInCentiMetres(){
+//     document.write("<h1> The " + distance + " KM is equal to " + distance * 100000 + " Centimetres.</h1>");
+// }
+// kmInCentiMetres();
+// function salaryCalculation(a,b){
+//     document.write("<h1> The overtime of " + a + " hours is " + a*b + "</h1>");
+
+// }
+// salaryCalculation(40,12);
+// salaryCalculation(56,14);
